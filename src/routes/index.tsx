923d9f9400fef227d15fc1d9f6765f2e9852ea3a
heenriquesilver <luizhenriquechaves@outlook.com.br>
2024-10-05 15:52:19 -0300

    feat: Adicionado contexto de temas claro e escuro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,25 @@
 import { Button } from "@mui/material";
+
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
+import { useContext } from "react";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
             Teste
           </Button>
         }
       />
 
       <Route path="*" element={<Navigate to={"/pagina-inicial"} />} />
+      <Route path="/outra-rota" element={<h1>Outra Rota</h1>} />
     </Routes>
   );
 };
